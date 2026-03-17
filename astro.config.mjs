6de6da3ca723858679b8d2714651af8b10ea903d
Ramona <rmn.neagu@yahoo.ro>
2026-03-17 23:46:29 +0200

    Updated astro.confif.msj - deleted output: server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://phayze2184.github.io',
   base: '/isaf',
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
   },
 });
-
-
 
